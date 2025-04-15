941f1d058e5ae8d209475e17470802604d755098
devevandro <dev.evandro@gmail.com>
2025-04-14 21:24:57 -0300

    add login route to application routing

--- src/renderer/routes.tsx
+++ src/renderer/routes.tsx
@@ -3,7 +3,13 @@ import { Route } from "react-router-dom";
 import { Router } from "lib/electron-router-dom";
 
 import CalendarPage from "./screens/main";
+import LoginPage from "./screens/login";
 
 export function AppRoutes() {
-  return <Router main={<Route path="/" element={<CalendarPage />} />} />;
+  return (
+    <Router
+      main={<Route path="/" element={<CalendarPage />} />}
+      about={<Route path="/:login" element={<LoginPage />} />}
+    />
+  );
 }
