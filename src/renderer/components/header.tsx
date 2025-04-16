a18f4e363752b80f66ee0f315c901535739f6200
Evandro Carvalho Ferreira <dev.evandro@gmail.com>
2025-04-16 13:38:43 -0300

    feat: improvement header

--- src/renderer/components/header.tsx
+++ src/renderer/components/header.tsx
@@ -17,18 +17,11 @@ export default function Header() {
 
   if (!mounted) return null;
 
-  const isDarkMode = true;
-
   return (
     <header
-      className={`draggable pb-2 w-full ${
-        isDarkMode ? "bg-gray-800 border-gray-700" : "bg-white border-gray-200"
-      } border-b shadow-sm sticky top-0 z-10`}
+      className={`draggable pb-2 w-full bg-gray-800 border-gray-700 border-b shadow-sm sticky top-0 z-10`}
     >
-      <div className="container mx-auto px-4 py-3 flex items-center justify-between">
-        {/* Menu mobile toggle */}
-        <div className="md:hidden flex items-center"></div>
-      </div>
+      <div className="container mx-auto px-4 py-3" />
     </header>
   );
 }
