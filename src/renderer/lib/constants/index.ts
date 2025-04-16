71a25ea647b79235de8b2557628d311e9a34f197
devevandro <dev.evandro@gmail.com>
2025-04-15 22:03:16 -0300

    add constants for months and weekdays in calendar

---
+++ src/renderer/lib/constants/index.ts
@@ -0,0 +1,16 @@
+export const months = [
+  "Jan",
+  "Fev",
+  "Mar",
+  "Abr",
+  "Mai",
+  "Jun",
+  "Jul",
+  "Ago",
+  "Set",
+  "Out",
+  "Nov",
+  "Dez",
+];
+
+export const weekdays = ["DOM", "SEG", "TER", "QUA", "QUI", "SEX", "SAB"];
